6e7c63e580f853067cbfffa83628938469631294
Niraj <v-nikhan@microsoft.com>
2015-12-16 15:03:36 +0530

    changes as on 1216

--- Static/Scripts/layout.js
+++ Static/Scripts/layout.js
@@ -180,12 +180,65 @@ function redirect() {
     } else {
         loadPage('Main');
         document.title = 'Digital Marketing | Analytics | Business Intelligence | CRM | Mobile | Azure application and solution development - MAQ Software';
+        loadNewsMainPage();
         $('#dvHeader').css('border-bottom', 'none');
     }
+}
+function loadNewsMainPage() {
+    $.ajax({
+        url: 'http://www.blogger.com/feeds/2523158019509365490/posts/default/-/News',
+        type: 'GET',
+        dataType: 'jsonp',
+        success: function (sResponse) {
+            loadNewsMain(sResponse);
+        }//,
+        //complete: function () {
+        //    oNewsContainer.removeClass(sLoadingClass);
+        //}
+    });
+}
+function loadNewsMain(sNewsData) {
+    try {
+        // var iMaxPageIndex;
+        var parser = new DOMParser();
+        oNewsData = parser.parseFromString(sNewsData, "text/xml");
+        iTotalNews = oNewsData.getElementsByTagName('entry').length;
+        // if (iTotalNews || oNewsData.getElementsByTagName('content')) {
+        //   iMaxPageIndex = Math.round(iTotalNews / oNewsPager.pagesize);
+        // $("#Pagination").show();
+        renderNewsMain();
+        //}
+    } catch (ignore) {
+    }
+}
+function renderNewsMain() {
+
+    var iStart, iEnd, entry1, sDate, oDatePart, oDate, iTotalNews = 0;
+    entry1 = oNewsData.getElementsByTagName('entry').item(0);
+    if (entry1) {
+        sDate = entry1.getElementsByTagName('published')[0].childNodes[0].nodeValue.toLowerCase().split("t");
+        oDatePart = [];
+        if (sDate[0]) {
+            oDatePart = sDate[0].split("-");
+        }
+        oDate = null;
+        if (oDatePart.length === 3) {
+            oDate = new Date(oDatePart[0], (oDatePart[1] - 1), oDatePart[2]);
+        } else {
+            oDate = new Date(sDate[0]);
+        }
+        oDate = oDate.format();
+        $("#dvContent2 .pSubContent:first").text(oDate);
+        $("#dvContent2 .pContent .SemiBold").text(entry1.getElementsByTagName('title')[0].childNodes[0].nodeValue);
+        $("#dvContent2 .pContent .subData").html(entry1.getElementsByTagName('content')[0].childNodes[0].nodeValue);
+    }
+    $('.subData *').removeAttr('style');
+    $('.subData').dotdotdot();
 }
 
 $(document).ready(function () {
     "use strict";
+
     $('.year').text(new Date().getFullYear());
     redirect();
 
@@ -193,3 +246,4 @@ $(document).ready(function () {
         redirect();
     };
 });
+
